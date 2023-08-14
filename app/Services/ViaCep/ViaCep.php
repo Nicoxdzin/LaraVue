7b6f4fde372e8f9d71e2e4e3a028bdcb8f075c00
Nicoxdzin <nimarques1207@gmail.com>
2023-08-14 03:22:32 -0300

    WithoutVerifying

--- app/Services/ViaCep/ViaCep.php
+++ app/Services/ViaCep/ViaCep.php
@@ -22,6 +22,7 @@ class ViaCep
     {
         $this->request = Http::asJson()
             ->acceptJson()
+            ->withoutVerifying()
             ->baseUrl($this->config['base_url']);
     }
 }
